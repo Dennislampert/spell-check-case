f89aa7ff73bf50fdc61d61ab7aec0469c630910a
Lampert <dennis@lampert.se>
2017-01-21 16:13:22 +0100

    start using git for this version handling

---
+++ spec/spell-check-camle-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SpellCheckCamleView from '../lib/spell-check-camle-view';
+
+describe('SpellCheckCamleView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
